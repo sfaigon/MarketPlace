39193e93a37a99b54d06919b858cae6759542271
An Nguyen <sfaigon@gmail.com>
2023-10-30 11:16:39 -0400

    create simple market model

---
+++ models/markets.js
@@ -0,0 +1,13 @@
+const mongoose = reuire("mongoose");
+const Schema = mongoose.Schema;
+
+const marketSchema = new Schema({
+    name: {type: String, required: true},
+    location: {type: String, required: true},
+    // categories - debating setup
+    //rating - add later
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Market", marketSchema);
