103bb917d601cb71152a720683cb37a06bc26684
An Nguyen <sfaigon@gmail.com>
2023-10-30 15:39:52 -0400

    add show function to markets controller

--- controllers/markets.js
+++ controllers/markets.js
@@ -1,12 +1,18 @@
-const Market = require("../models/markets");
+const Market = require("../models/market");
 module.exports = {
   index,
   new: newMarket,
   create,
+  show,
 };
 async function index(req, res) {
   const markets = await Market.find({});
   res.render("markets/index", { title: "All Markets", markets });
+}
+
+async function show(req, res){
+    const market = await Market.findById(req.params.id);
+    res.render("markets/show", {title: `${market.name}`, market});
 }
 
 function newMarket(req, res) {
--- routes/markets.js
+++ routes/markets.js
@@ -5,6 +5,7 @@ const marketsCtrl = require("../controllers/markets");
 /* GET users listing. */
 router.get('/', marketsCtrl.index);
 router.get('/new', marketsCtrl.new);
+router.get("/:id", marketsCtrl.show);
 router.post('/', marketsCtrl.create);
 
 module.exports = router;
