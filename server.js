4437b54eb9784b729b44d7cf4ab4413a28d51f3e
An Nguyen <sfaigon@gmail.com>
2023-10-30 11:05:26 -0400

    change marketplaces to markets for shortness, change users to markets and ajust server.js to reflect change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require('./config/passport');
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var marketsRouter = require('./routes/markets');
 
 var app = express();
 
@@ -49,7 +49,7 @@ app.use(function (req, res, next) {
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/markets', marketsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
