e357da6bf3b8bb7e699b8f249de373a3e3dc8755
An Nguyen <sfaigon@gmail.com>
2023-10-30 14:50:14 -0400

    create listingSchema and add to marketSchema, will embed listings

--- models/markets.js
+++ models/markets.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const listingSchema = new Schema({
+    item: { type: String, required: true },
+    price: { type: Number, min : 1, required: true},
+    quantity: {type: Number, min: 1, required: true},
+}, {
+    timestamps: true
+})
+
 const marketSchema = new Schema({
     name: {type: String, required: true},
     location: {type: String, required: true},
     // categories - debating setup
     //rating - add later
+    listings: [listingSchema],
 }, {
     timestamps: true
 });
+
 
 module.exports = mongoose.model("Market", marketSchema);
