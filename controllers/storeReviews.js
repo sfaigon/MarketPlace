fe849e73425b10a152dc79aa74857ed673fe85d7
An Nguyen <sfaigon@gmail.com>
2023-10-31 11:07:12 -0400

    code deleteStoreReviews function in storeReviews controller

--- controllers/storeReviews.js
+++ controllers/storeReviews.js
@@ -2,6 +2,7 @@ const Market = require("../models/market");
 
 module.exports = {
   create,
+  delete: deleteStoreReview,
 };
 
 async function create(req, res) {
@@ -13,4 +14,16 @@ async function create(req, res) {
     console.log(err);
   }
   res.redirect(`/markets/${market._id}`);
+}
+
+function deleteStoreReview(req, res, next){
+  Market.findOne({"storeReviews._id": req.params.id}).then(function(market){
+    if(!market) return res.redirect("/markets");
+    market.storeReviews.remove(req.params.id);
+    market.save().then(function(){
+      res.redirect(`/markets/${market._id}`);
+    }).catch(function(err){
+      return next(err);
+    })
+  })
 }
