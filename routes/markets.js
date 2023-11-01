e4ee76aec45bea8d11b347682954f0892cc0e599
An Nguyen <sfaigon@gmail.com>
2023-10-31 20:00:49 -0400

    implement user delete if it is their own post for reviews and listings

--- routes/markets.js
+++ routes/markets.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const marketsCtrl = require("../controllers/markets");
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 /* GET users listing. */
 router.get('/', marketsCtrl.index);
 router.get('/new', marketsCtrl.new);
 router.get("/:id", marketsCtrl.show);
-router.post('/', marketsCtrl.create);
+router.post('/', ensureLoggedIn, marketsCtrl.create);
 
 module.exports = router;
